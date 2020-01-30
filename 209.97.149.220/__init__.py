44dc330397c4b3a56a566c9898ba21bb4a8f3b64
phuang00 <phuang00@stuy.edu>
2020-01-30 08:40:37 -0500

    updated flask file

--- 209.97.149.220/__init__.py
+++ 209.97.149.220/__init__.py
@@ -14,8 +14,8 @@ DIR += '/'
 
 @app.route("/")
 def root():
-    return ""
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
